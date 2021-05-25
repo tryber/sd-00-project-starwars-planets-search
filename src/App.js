dbc757b09776ed03cc984c81fd514e77739c9e9e
Tiago <tiagaoalb@gmail.com>
2021-05-25 09:03:47 -0300

    normal commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ActiveFilters from './components/ActiveFilters';
+// import ActiveFilters from './components/ActiveFilters';
 import FilterForm from './components/FilterForm';
 import Table from './components/Table';
 import PlanetsProvider from './context/PlanetsProvider';
